0d71946c7378f252ddc378f8cd6255b4aa7e675e
John Kohl <jtk.git@bostonpog.org>
2023-10-23 19:22:23 -0400

    logging to debug things, and cleanups

--- applications/luci-app-statistics/root/lib/upgrade/luci_statistics-add-conffiles.sh
+++ applications/luci-app-statistics/root/lib/upgrade/luci_statistics-add-conffiles.sh
@@ -2,16 +2,10 @@ add_luci_statistics_conffiles()
 {
 	local filelist="$1"
 	# create a backup if needed
-	local rrd_enabled=$(uci -q get luci_statistics.collectd_rrdtool.enable)
-	local rrd_backups_enabled=$(uci -q get luci_statistics.collectd_rrdtool.backup)
-	local rrd_dir=$(uci -q get luci_statistics.collectd_rrdtool.DataDir)
-
-	### Backups are enabled if RRD is enabled, backups enabled, and the data directory name is non-empty
-	[ "$rrd_enabled" = "1" \
-		 -a "$rrd_backups_enabled" = "1" \
-		 -a -n "$rrd_dir" ] && {
+	/etc/init.d/luci_statistics backups_enabled && {
 		echo "/etc/luci_statistics/rrdbackup.tgz" >>$filelist
-		### If statistics are running, trigger a backup now
+		### If statistics are running, trigger a backup now.
+		### (If not running, then it saved backups already)
 		/etc/init.d/luci_statistics status >/dev/null 2>&1 && /etc/init.d/luci_statistics backup
 	}
 }
